5e31c29968351d7f31566768254a05a5a84a64f1
Ben PS <bentops@gmail.com>
2016-03-12 17:55:39 +0000

    finnish with og

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ $(document).ready(function()
 		else if(this.id == "4") window.open("http://bentops.azurewebsites.net/06_googleAPI/googleAPI.html" ,'_blank'); 
 		else if(this.id == "5") window.open("http://bentops.azurewebsites.net/07_resume/resume.html" ,'_blank');
 	});
+	$(".content > div").hover(function(){
+		$(this).css({
+			'background-color': 'lightskyblue',
+			'transition': '0.5s'
+		});
+		$('#' + this.id + "> .classno > h3").css({
+			'color': 'white',
+			'transition': '0.5s'
+		});
+	},function(){
+		$(this).css({
+			'background-color': 'white'
+		});
+		$('#' + this.id + "> .classno > h3").css({
+			'color': 'lightskyblue'
+		});
+	});
+	$(".header > .h1 > h1").click(function(){
+		window.open("http://bentops.azurewebsites.net/07_resume/resume.html" ,'_blank');
+	});
 });
 
 
